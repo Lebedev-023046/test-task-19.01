f9b1e668c926ef56ffe00d96c0f62d07272a0db0
lebedev-023046 <lebedev.023046@gmail.com>
2026-01-20 20:41:15 +0300

    feat: add toast

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,21 @@
+import { Toaster } from 'react-hot-toast'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 
 export function App() {
-	return <RouterProvider router={router} />
+	return (
+		<>
+			<RouterProvider router={router} />
+			<Toaster
+				position='bottom-right'
+				toastOptions={{
+					style: {
+						fontSize: '1.8rem',
+						padding: '1rem 2rem',
+						borderRadius: 'var(--rounded-md)'
+					}
+				}}
+			/>
+		</>
+	)
 }
